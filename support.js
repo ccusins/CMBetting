acfa2d979d1285eb70af578bebc27fa64ac97b7c
ccusins <157019744+ccusins@users.noreply.github.com>
2024-01-18 21:19:40 +0000

    Update support.js

--- support.js
+++ support.js
@@ -354,7 +354,7 @@ function getUsers(token) {
 
 document.addEventListener('DOMContentLoaded', function() { 
     
-    const token = API_KEY;
+    const token = "f72754b3-cd78-4472-990a-22f581bf10c2";
     getUsers(token);
     setUpDepositListener(token);
     setUpAccountListener(token);
